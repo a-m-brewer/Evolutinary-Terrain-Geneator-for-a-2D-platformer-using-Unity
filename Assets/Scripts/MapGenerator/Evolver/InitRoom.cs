189de7a36f6e3f09aa2575c17bd4cd0e855b39ba
Adam Brewer <ambreweruni@gmail.com>
2018-02-08 19:28:06 +0000

    map stuff wip

--- Assets/Scripts/MapGenerator/Evolver/InitRoom.cs
+++ Assets/Scripts/MapGenerator/Evolver/InitRoom.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class CreateRoom {
 
+    private const int ERROR_NO_TILE = -1;
+    private const int BACKGROUND = 0;
+    private const int GROUND = 1;
+    private const int GAP = 6;
+    private const int SPAWN_POINT_INDEX = 2;
+
+    private float[] chanceOfSpawning = new float[6]
+    {
+        40f, // ground
+        20f, // gap
+        20f, // coin
+        20f, // trap
+        10f, // enemy
+        5f   // enemy with gun
+    };
 
     public int[] Generate()
     {
@@ -11,16 +26,53 @@ public class CreateRoom {
 
         for (int i = 0; i < randomRoom.Length; i++)
         {
-            randomRoom[i] = GetRandomTile();
+            randomRoom[i] = SetTile(i);
+
+
         }
 
-        Debug.Log(randomRoom.Length);
-
         return randomRoom;
+    }
+
+    private int SetTile(int index)
+    {
+        int toPlace = ERROR_NO_TILE;
+        for (int i = 0; i < chanceOfSpawning.Length; i++)
+        {
+            int tileType = i + 1;
+            toPlace = TryPlaceTile(chanceOfSpawning[0], tileType, toPlace);
+        }
+
+        if (index < TileInformation.roomSizeX && toPlace == ERROR_NO_TILE)
+            toPlace = GAP;
+
+        if (toPlace == ERROR_NO_TILE)
+            toPlace = BACKGROUND;
+
+        if (index == SPAWN_POINT_INDEX)
+            toPlace = GROUND;
+
+        return toPlace;
+    }
+
+    private int TryPlaceTile(float chance, int tileType, int prevValue)
+    {
+        if(RandomChance(chance) && prevValue == ERROR_NO_TILE)
+        {
+            return tileType;
+        }
+        // return a number that is not a tile
+        return prevValue;
     }
 
     private int GetRandomTile()
     {
         return Random.Range(0, TileInformation.difficultyScores.Length - 1);
     }
+
+    private bool RandomChance(float chance)
+    {
+        return (Random.Range(0, 100) <= chance);
+    }
+
 }
---
+++ Assets/TextFileWriter.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class TextFileWriter {
+
+    // TODO: Write map data to a text file to see wtf is going on
+
+    string path = System.IO.Path.GetFullPath(".");
+    string filename = "default.txt";
+    StreamWriter sr;
+
+    public TextFileWriter(string filename)
+    {
+        filename = filename + ".txt";
+    }
+
+    public void OpenStream()
+    {
+        sr = File.CreateText(path + filename);
+    }
+
+    public void CloseStream()
+    {
+        sr.Close();
+    }
+
+    public void WriteLine(string line)
+    {
+        if(sr == null)
+        {
+            Debug.Log("File stream not open");
+            return;
+        }
+
+        sr.WriteLine(line);
+    }
+
+}
