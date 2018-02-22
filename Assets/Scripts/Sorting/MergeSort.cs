539fdb4beb68aa21af8b1b1b0a39c8f673948397
a-m-brewer <a-m-brewer@users.noreply.github.com>
2018-02-22 13:26:58 +0000

    Merge Sort

--- Assets/Scripts/Sorting/MergeSort.cs
+++ Assets/Scripts/Sorting/MergeSort.cs
@@ -1,8 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MergeSortRoom {
 
     // TODO: Make a merge sort based on fitness score
+    public List<Room> MergeSort(List<Room> roomList)
+    {
+        if(roomList.Count == 1)
+        {
+            return roomList;
+        }
+        List<Room> listOne = roomList.Take(roomList.Count / 2).ToList();
+        List<Room> listTwo = roomList.Skip(roomList.Count / 2).ToList();
+
+        listOne = MergeSort(listOne);
+        listTwo = MergeSort(listTwo);
+
+        return Merge(listOne, listTwo);
+    }
+
+    public List<Room> Merge(List<Room> listOne, List<Room> listTwo)
+    {
+        List<Room> listC = new List<Room>();
+
+        while(listOne.Count > 0 && listTwo.Count > 0)
+        {
+            if(listOne[0].Fitness > listTwo[0].Fitness)
+            {
+                listC.Add(listTwo[0]);
+                listTwo.Remove(listTwo[0]);
+            } else
+            {
+                listC.Add(listOne[0]);
+                listOne.Remove(listOne[0]);
+            }
+        }
+
+        while(listOne.Count > 0)
+        {
+            listC.Add(listOne[0]);
+            listOne.Remove(listOne[0]);
+        }
+
+        while(listTwo.Count > 0)
+        {
+            listC.Add(listTwo[0]);
+            listTwo.Remove(listTwo[0]);
+        }
+
+        return listC;
+    }
 }
--- Assets/Scripts/Sorting/MergeTesting.cs
+++ Assets/Scripts/Sorting/MergeTesting.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MergeTesting : MonoBehaviour {
@@ -9,7 +10,24 @@ public class MergeTesting : MonoBehaviour {
 	void Start () {
         InitHuristicRooms ihr = new InitHuristicRooms(huristicMaps, new EvaluateRoom(0.75f));
         Room[] hRooms = ihr.Rooms;
-        
+
+        foreach(Room r in hRooms)
+        {
+            r.Fitness = Random.Range(0, 99);
+            Debug.Log(r.Fitness);
+        }
+
+        Debug.Log("Random Ends");
+
+        List<Room> hRoomList = hRooms.ToList();
+
+        MergeSortRoom msr = new MergeSortRoom();
+        List<Room> merge = msr.MergeSort(hRoomList);
+
+        foreach(Room r in merge)
+        {
+            Debug.Log(r.Fitness);
+        }
     }
 	
 }
